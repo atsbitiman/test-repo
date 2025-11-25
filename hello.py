faa2a2da45845962da020d876a2be6beb21bc628
= <=>
2025-11-25 21:00:42 +0700

    Menambah metode dalam string

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print("Hello World")
+var = 'kelurahan kebonagung'
+
+print(len(var))
+print(var.upper())
+print(var.lower())
+print(var.count('a'))
+print(var.find('b'))
+print(var.replace('b','z'))
+print(var.capitalize())
+print(var.title())
