be6ad08908804d7dfa59fba609d8e0695f15333b
= <=>
2025-11-26 21:03:48 +0700

    Berlajar Compound Data Structure

--- hello.py
+++ hello.py
@@ -9,3 +9,8 @@ print(var.find('b'))
 print(var.replace('b','z'))
 print(var.capitalize())
 print(var.title())
+
+profit = [5_000_000, 7_500_000, 3_500_000, 10_000_000, 
+          15_000_000,9_000_000, 11_000_000, 13_000_000]
+print(profit[3:-2])
+
