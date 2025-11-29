1b63cfa99af10079a89f8f601673a5640554f3f1
= <=>
2025-11-29 21:21:58 +0700

    Menambahkan file konversi_hari.py, bunga_bank.py dan main.py

---
+++ bunga_bank.py
@@ -0,0 +1,13 @@
+#fungsi bunga terendah
+def bunga_terendah(saldo, bunga):
+  hari = 30
+  tahun = 365
+  tabungan = saldo * ((bunga * 0.01) * hari) / tahun
+  return round(tabungan,2)
+
+
+# fungsi bunga anuitas
+def bunga_anuitas(pinjaman, bunga):
+  hari = 30
+  tabungan = pinjaman * bunga * hari
+  return tabungan
---
+++ main.py
@@ -0,0 +1,7 @@
+#impor modul
+import bunga_bank
+
+#memanggil fungsi jam dari modul konversi_hari
+anuitas_1 = bunga_bank.bunga_anuitas(2000000, 10)
+
+print(f"Bunga Anuitas yang didapatkan adalah Rp {anuitas_1}")
